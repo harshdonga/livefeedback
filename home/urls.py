5c45b73fd60f30dc6fed927c48c191ea04d82528
Harsh Donga <harshdonga99@gmail.com>
2019-03-22 21:09:37 +0530

    second commit

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('faculty/' , include('faculty.urls')),
     path('student/' , include('student.urls')),
     path('api/' , include('faculty.api_urls')),
-    path('api/' , include('student.api_urls'))
+    path('api/' , include('student.api_urls')),
+    path('gallery/' , views.gallery , name = "gallery"),
+    path('index/' , views.index , name = "index"),
+    path('contact/' , views.contact , name = "contact"),
+    path('about/' , views.about , name = "about")
 
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request , 'home/index.html')
+
+def contact(request):
+    return render(request , 'home/contact.html')
+
+def gallery(request):
+    return render(request , 'home/gallery.html')
+
+def about(request):
+    return render(request , 'home/about.html')
